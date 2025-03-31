9ba79d0567e8785618201553f5c95566d8b9a1c8
beatrizmarques2005 <beatriz.soares.marques.pt@gmail.com>
2025-03-31 15:35:58 +0100

    duplicates and new doc

--- source_code/preprocessing.py
+++ source_code/preprocessing.py
@@ -1,9 +1,57 @@
 import pandas as pd
 import numpy as np
 
-#MISSING VALUES
+import pandas as pd
 
-#function for manually imputing for loyalty card
+######### GENERAL EXPLORATION #########
+
+def initial_exploration(data):
+    
+    # Display the first few rows of the dataset
+    print("First few rows of the dataset:")
+    print(data.head())
+    
+    # Display the shape of the dataset
+    print("\nShape of the dataset:")
+    print(data.shape)
+    
+    # Display the data types of each column
+    print("\ndata types of each column:")
+    print(data.dtypes)
+    
+    # Display basic statistics of the dataset
+    print("\nBasic statistics of the dataset:")
+    print(data.describe().T)
+    
+    # Check for missing values
+    print("\nMissing values in each column:")
+    print(data.isnull().sum())
+
+######### CUSTOMER_BASKET #########
+
+def correct_customer_backet_format(df):
+    pass
+
+
+######### DUPLICATES #########
+
+def check_duplicates(data):
+    duplicate_count = data.duplicated().sum()
+    print(f"Number of duplicate rows: {duplicate_count}")
+    return duplicate_count
+
+def treat_duplicates(data):
+    data = data.drop_duplicates()
+    print("Duplicates have been removed.")
+    return data
+
+
+######### OUTLIERS #########
+
+
+
+######### MISSING VALUES #########
+
 def impute_loyalty_card(data):
     data['loyalty_card_number'].replace(np.nan, 0, inplace = True)
     return data
@@ -24,3 +72,12 @@ def impute_teens_kids_home(data):
 
 def impute_lifetime_spend_alcohol_drinks(data):
     data['lifetime_spend_alcohol_drinks'].replace(np.nan, 0, inplace = True)
+
+
+######### ENCODING #########
+
+
+######### INCONSISTENCIES #########
+
+
+######### SCALING #########
---
+++ source_code/utils.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+
+
+def load_dataset(dataset_path):
+
+    if dataset_path.endswith('.csv'):
+        data = pd.read_csv(dataset_path)
+    
+    elif dataset_path.endswith('.xlsx'):
+        data = pd.read_excel(dataset_path)
+    
+    else:
+        raise ValueError("The dataset must be in CSV or EXCEL format.")
+    
+    return data
+
+
+def save_dataset(data, format, path):
+
+    if format == 'csv':
+        data.to_csv(path, index = False)
+
+    elif format == 'xlsx':
+        data.to_excel(path, index = False)
+
+    else:
+        raise ValueError("The format must be in CSV or EXCEL.")
