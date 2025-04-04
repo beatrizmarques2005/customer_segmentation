2b6db306035933babeb693630006f0fd0c051f4f
beatrizmarques2005 <beatriz.soares.marques.pt@gmail.com>
2025-04-04 14:13:46 +0100

    kmeans

--- source_code/clustering.py
+++ source_code/clustering.py
@@ -1,1 +1,9 @@
-import pandas as pd
+from sklearn.cluster import KMeans
+
+def kmeans_clustering(data, n_clusters=5):
+
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+
+    data['cluster'] = kmeans.fit_predict(data)
+
+    return data
