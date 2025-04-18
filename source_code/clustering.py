66bbb309766ec229e43ce3d07674eeb69826c2b2
Dari212 <darija078@gmail.com>
2025-04-18 13:43:25 +0100

    added hierarchical clustering

--- source_code/clustering.py
+++ source_code/clustering.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 
 def kmeans_clustering(data, n_clusters=5):
 
@@ -7,3 +8,11 @@ def kmeans_clustering(data, n_clusters=5):
     data['cluster'] = kmeans.fit_predict(data)
 
     return data
+
+
+def hier_clustering(data, linkage = 'ward'):
+    hier = AgglomerativeClustering(distance_threshold= 0, linkage=linkage, n_clusters=None, compute_full_tree=True)
+    data['hier'] = hier.fit_predict(data)
+    return data 
+
+
