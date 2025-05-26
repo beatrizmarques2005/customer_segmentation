b00227ddadad883003a373f3c27d286ebb83247d
Dari212 <darija078@gmail.com>
2025-05-26 18:02:23 +0100

    few changes

--- source_code/clustering.py
+++ source_code/clustering.py
@@ -125,6 +125,8 @@ def kmeans_clustering(data: pd.DataFrame, data_scaled: pd.DataFrame,  n_clusters
 
     data['KMeans'] = kmeans.fit_predict(data_scaled)
 
+    silhouette_score = silhouette_score(data_scaled, kmeans.fit_predict(data_scaled))
+    print(f"KMeans Silhouette Score: {silhouette_score:.4f}")
     return data
 
 def plot_elbow(data: pd.DataFrame, max_k: int = 10) -> None:
